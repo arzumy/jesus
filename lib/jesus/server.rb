45e4b25eb23cc9a9c7811f20113bfbd5885928fd
dmathieu <42@dmathieu.com>
2009-11-10 18:54:54 +0100

    view rendering now works

--- lib/jesus/server.rb
+++ lib/jesus/server.rb
@@ -3,7 +3,11 @@ require 'sinatra_more/render_plugin'
 
 module Jesus
   class Server < Sinatra::Base
+    dir = File.dirname(File.expand_path(__FILE__))
     register SinatraMore::RenderPlugin
+    set :views, "#{dir}/server/views"
+    set :public, "#{dir}/server/public"
+    set :static, true
     
     #
     # The home
